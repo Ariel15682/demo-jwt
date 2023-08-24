91cdd9e3a2abca660d0960b2d85288693596752f
ariel <arieldiaz05@gmail.com>
2023-08-24 13:05:05 +0200

    Creacion data.sql scripts para inicializar datos en BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_USER');
+
+INSERT INTO cars (manufacturer, model, cc, doors, año, release_date, available) VALUES ('VW', 'Model', 1.6, 4, 1986, '1986-01-30', true);
+INSERT INTO cars (manufacturer, model, cc, doors, año, release_date, available) VALUES ('Renault', 'Kangoo', 1.8, 5, 2008, '2008-06-15', true);
+
+--INSERT INTO users(username, email, password, roles) VALUES ('ariel', 'ariel@mail.es', '1234567', 'user');
+
