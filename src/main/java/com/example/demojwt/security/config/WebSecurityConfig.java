03caefd49b116ca9f9e63e35da15290acdc8b317
ariel <arieldiaz05@gmail.com>
2023-08-24 14:25:06 +0200

    Creacion clase WebSecurityConfig

---
+++ src/main/java/com/example/demojwt/security/config/WebSecurityConfig.java
@@ -0,0 +1,148 @@
+package com.example.demojwt.security.config;
+
+import com.example.demojwt.security.jwt.AuthEntryPointJwt;
+import com.example.demojwt.security.jwt.AuthTokenFilter;
+import com.example.demojwt.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.http.HttpMethod;
+
+
+/**
+ *  Clase para la configuracion de seguridad Spring Security
+ */
+@Configuration
+//@EnableWebSecurity // Permite a Spring aplicar esta configuracion a la configuraicon de seguridad global
+@EnableMethodSecurity
+// (securedEnabled = true,
+// jsr250Enabled = true,
+// prePostEnabled = true) // by default
+public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.h2.console.path}")
+    private String h2ConsolePath;
+
+//  private static final String[] AUTH_WHITE_LIST = {
+//            "/**"
+//  };
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+//  @Override
+//  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//  }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+//  @Bean
+//  @Override
+//  public AuthenticationManager authenticationManagerBean() throws Exception {
+//    return super.authenticationManagerBean();
+//  }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Cross-site Request Forgery CSRF
+        // CORS (Cross-Origin-Resource-Sharing)
+        http
+                // Enable CORS
+                //.cors(Customizer.withDefaults())
+                //Disable CSRF
+                //.csrf((protection) -> protection.ignoringRequestMatchers(toH2Console())
+                .csrf(AbstractHttpConfigurer::disable) //.csrf(csrf -> csrf.disable())
+                // Set unauthorized requests exception handler
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                // Set session management to stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Set permission solo JWT
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                //.requestMatchers(HttpMethod.POST,"/api/auth/signin/**").permitAll()
+                                .requestMatchers("/api/auth/signin/**").permitAll()
+                                .requestMatchers(antMatcher(h2ConsolePath + "/**")).permitAll()
+                                //.requestMatchers("/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll() // Swagger 3
+                                //.requestMatchers(AUTH_WHITE_LIST).permitAll()
+                                .anyRequest().authenticated() //el resto esta capado
+                );
+
+        // This will allow frames with same origin which is much more safe
+        // fix H2 database console: Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // (Lambda) (frameOption -> frameOption.sameOrigin()));
+
+        http.authenticationProvider(authenticationProvider());
+
+        // Add JWT token filter
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher(h2ConsolePath));
+//    }
+
+//    /**
+//     * Configuracion global de CORS para toda la aplicacion
+//     */
+//    // Prohibir que se acceda a nuestro Backend desde determinados dominios y solo desde el que queremos
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource(){
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        //configuration.setAllowedOrigins(List.of("http://localhost:8080", "https://angular-springboot-beta.vercel.app"));
+//        //configuration.setAllowedOriginPatterns(List.of("http://localhost:8080", "https://angular-springboot-beta.vercel.app"));
+//        configuration.setAllowedOriginPatterns(List.of("http://localhost:8080"));
+//        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+//        configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested with", "Origins", "Content-Type", "Accept", "Authorization"));
+//        configuration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+}
