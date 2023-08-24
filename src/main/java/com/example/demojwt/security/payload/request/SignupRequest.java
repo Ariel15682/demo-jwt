dace27df20fbca81a076854dfd57d048666dbb9e
ariel <arieldiaz05@gmail.com>
2023-08-24 13:57:14 +0200

    Creacion clase SignupRequest

---
+++ src/main/java/com/example/demojwt/security/payload/request/SignupRequest.java
@@ -0,0 +1,54 @@
+package com.example.demojwt.security.payload.request;
+
+import java.util.Set;
+import jakarta.validation.constraints.*;
+
+//No valida la cuenta por correo ya que necesitariamos mas herramientas; plantilla http en thymeleaf, un servidor smtp
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+}
